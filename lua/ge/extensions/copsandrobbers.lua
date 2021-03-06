89e5f28c30a253b293a3f89f9713a278ed34bb2e
Maikel N <7192304+Iskelmaikel@users.noreply.github.com>
2021-03-06 13:02:12 +0100

    initial commit

---
+++ lua/ge/extensions/copsandrobbers.lua
@@ -0,0 +1,19 @@
+
+local M = {}
+
+local resource
+
+local function onExtensionLoaded()
+    resource = jsonReadFile("settings/resource.json")
+    log("I", resource.name,  "version " .. resource.version .. " loaded")
+end
+
+local function onExtensionUnloaded()
+    log("I", resource.name,  "version " .. resource.version .. " unloaded, goodbye!")
+    Lua:requestReload()
+end
+
+M.onExtensionLoaded = onExtensionLoaded
+M.onExtensionUnloaded = onExtensionUnloaded
+
+return M
---
+++ modScript.lua
@@ -0,0 +1,2 @@
+load("copsandrobbers")
+registerCoreModule("copsandrobbers")
