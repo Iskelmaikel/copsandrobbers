fa5d9be91af893de5523b60c81dfe6be2d3df810
Maikel N <7192304+Iskelmaikel@users.noreply.github.com>
2021-03-09 23:47:55 +0100

    Add files via upload

--- lua/ge/extensions/copsandrobbers.lua
+++ lua/ge/extensions/copsandrobbers.lua
@@ -6,26 +6,36 @@
 
 local M = {}
 
-M.dependencies = {"ui_imgui"}
+--M.dependencies = {"ui_imgui"}
 
-local gui_module = require("ge/extensions/editor/api/gui")
-local gui = {setupEditorGuiTheme = nop}
-local imgui = ui_imgui
+-- local gui_module = require("ge/extensions/editor/api/gui")
+-- local gui = {setupEditorGuiTheme = nop}
+-- local imgui = ui_imgui
 
-log("Cops and robbers client initializing...")
+print("Cops and robbers client initializing...")
 
 local pluginAge = 0
 local resource
 local flagRadius = 3
-local flagHeight = 25
+local flagHeight = 100
 local newPositionBase = Point3F(13,160,0)
 local newPositionTop = Point3F(13,160,0 + flagHeight)
 local playerTeam = nil 
+local checkpointReached = false
+
+function split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
 
 -- Load additional config from json(?). Most settings will be handled serverside 
 local function onExtensionLoaded()
     resource = jsonReadFile("settings/resource.json")
-    log("I", "Cops and robbers", "version " .. resource.version .. " loaded")
+    print("Cops and robbers version " .. resource.version .. " loaded")
+    --log("I", "Cops and robbers", "version " .. resource.version .. " loaded")
 end
 
 local function onExtensionUnloaded()
@@ -37,12 +47,15 @@ local function distance(pos1, pos2)
 end
 
 -- Will be moved serverside
-local function setNewCheckpoint()
-    local newX = math.random(-200,200)
-    local newY = math.random(-200,200)
-
+local function setNewCheckpoint(data)
+    print("Received new checkpoint: " .. data)
+    data = data:sub(2)
+    local arr = split(data, ",")
+    local newX = tonumber(arr[1])
+    local newY = tonumber(arr[2])
     newPositionBase = Point3F(newX, newY, 0)
     newPositionTop = Point3F(newX, newY, 0 + flagHeight)
+    checkpointReached = false
 end
 
 -- Check if player has reached checkpoint
@@ -51,7 +64,7 @@ local function hasReachedCheckpoint()
         local veh = be:getObject(i)
         if MPVehicleGE.isOwn(veh:getID()) and distance(veh:getPosition(), newPositionBase) < flagRadius * 1.5
         then
-            setNewCheckpoint() -- will be replaced with call to server
+            TriggerServerEvent("CheckpointReached", "Client... reached the checkpoint. Requesting new one")
         end
     end
 end
@@ -59,7 +72,7 @@ end
 -- Spawn mission marker 
 local function drawCheckpoint()
     debugDrawer:drawCylinder(newPositionBase, newPositionTop, flagRadius, ColorF(0,0.6,0.7,0.5)) -- 2 colors? Red forcops and green for robbers to indicate good/bad?
-    guihooks.trigger("ScenarioChange", {name="Capture the flag rules:", description="break them rulez ok", introType="htmlOnly"})
+    --guihooks.trigger("ScenarioChange", {name="Capture the flag rules:", description="break them rulez ok", introType="htmlOnly"})
 end
 
 -- Main local game loop
@@ -68,7 +81,23 @@ local function onUpdate()
     hasReachedCheckpoint()
 end
 
+local function onPlayerJoinedTeam()
+    drawCheckpoint()
+    hasReachedCheckpoint()
+end
+local function onCheckpointReached()
+    TriggerServerEvent("CheckpointReached", "Player reached checkpoint. Requesting new one...")
+end
+
+local function onNewCheckpointReceived(data)
+    setNewCheckpoint(data)
+    checkpointReached = false
+end
+
+
 -- Register all event handlers
+AddEventHandler("PlayerJoinedTeam", onPlayerJoinedTeam)
+AddEventHandler("NewCheckpointReceived", onNewCheckpointReceived)
 AddEventHandler("StartGame", onStartGame)
 AddEventHandler("EndGame", onEndGame)
 AddEventHandler("ResetGame", onResetGame)
